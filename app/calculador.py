d1c44a9916cf7efb5c5d11fb64998d8ae4d11925
Julian Lozano <lozano.julian05@gmail.com>
2024-06-03 21:48:15 +0200

    Adding two new functions(% and log) in Calculadora.py

--- app/calculador.py
+++ app/calculador.py
@@ -12,16 +12,53 @@ class Calculador(object):
 
         """
         return self.__calculation_validation(calc=calc)
-
+    
     def __calculation_validation(self, calc):
         """Responsável por verificar se o calculo informado é possível ser feito"""
 
+        # divide o cálculo de entrada em componentes individuais com base em espaços em branco
+        components = calc.split()
+
+        modified_components = []
+
+        for component in components:
+            if component.startswith("log("):
+                # Se o componente for uma operação logarítmica, calcule-o separadamente
+                arg = component[4:-1]
+                
+                try:
+                    result = self.__calculate_log(float(arg))
+                    # Formate o resultado e adicione-o à lista de componentes modificados
+                    modified_components.append(str(result))
+                    
+                except (ValueError, ZeroDivisionError):
+                    return "Error"
+            else:
+                # Se o componente não for uma operação logarítmica, adicione-o inalterado
+                modified_components.append(component)
+        
+        # Join the modified components to form the modified expression
+        modified_expression = " ".join(modified_components)
+
         try:
-            result = eval(calc)
+            result = eval(modified_expression)
 
             return self.__format_result(result=result)
         except (NameError, ZeroDivisionError, SyntaxError, ValueError):
-            return 'Erro' 
+            return "Error"
+
+
+    def __calculate_log(self, value):
+        """Implementação personalizada do logaritmo natural (base-e)"""
+
+        result = 0
+        # Defina uma tolerância de erro
+        epsilon = 0.00001  
+        x = 1
+        while abs(x - value) > epsilon:
+            result += 1 / x
+            x *= value
+        return result
 
     def __format_result(self, result):
         """Formata o resultado em notação cientifica caso seja muito grande
